a6273161334d6ee1697a6822cbea98adca3cc029
leizleho <ho.leizle@gmail.com>
2019-04-14 00:01:38 -0700

    my first react hooks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello!</div>;
+  const [searchString, setSearchString] = useState('');
+
+  const updateSearchString = event => {
+    console.log('searchString', searchString);
+    setSearchString(event.target.value);
+  };
+  return (
+    <div className="App">
+      <h1>HELLO!</h1>
+      <div className="form">
+        <input value={searchString} onChange={updateSearchString} />
+        <button>Search</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
